e8bc5f973055f349b860dedd11a6a23fe53b491b
itylermcmanus <tyler.mcmanus1@icloud.com>
2016-10-03 08:52:11 -0500

    Add files via upload

---
+++ ClickEvents /js/script.js
@@ -0,0 +1,1014 @@
+$(document).ready(function() {
+
+	var kb=0;
+
+$(".kb").click(function(){
+      if (kb==0) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==1) {
+           $(this).css("background-color", "RGB(" + ad + "," + bd + "," + cd + ")");
+          kb++;
+      }
+      
+      else if (kb==2) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==3) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==4) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==5) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==6) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==7) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==8) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==9) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==10) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+
+      else if (kb==11) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==12) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==13) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==14) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==15) {
+         $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==16) {
+      	$(this).css("background-color", "#ff8c00 ");
+         
+         kb++;
+      }
+
+      else if (kb==17) {
+      	$(this).css("background-color", "#ff8c00 ");
+         
+         kb++;
+      }
+
+      else if (kb==18) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==19) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==20) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==21) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb=22) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==23) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==24) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==25) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==26) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==27) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+
+      else if (kb==28) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==29) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==30) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==31) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==32) {
+         $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==33) {
+      	$(this).css("background-color", "#ff8c00 ");
+         
+         kb++;
+      }
+
+      else if (kb==34) {
+      	$(this).css("background-color", "#ff8c00 ");
+         
+         kb++;
+      }
+
+      else if (kb==35) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==36) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==37) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==38) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==39) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==40) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==41) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==42) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==43) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==44) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+
+      else if (kb==45) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==46) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==47) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==48) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==49) {
+         $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==50) {
+      	$(this).css("background-color", "#ff8c00 ");
+         
+         kb++;
+      }
+
+      else if (kb==51) {
+      	$(this).css("background-color", "#ff8c00 ");
+         
+         kb++;
+      }
+
+
+      
+      else if (kb==52) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==53) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==54) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==55) {
+           $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+      else if (kb==56) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==57) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==58) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==59) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==60) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+
+      else if (kb==61) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==62) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==63) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==64) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==65) {
+         $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==66) {
+      	$(this).css("background-color", "#ff8c00 ");
+         
+         kb++;
+      }
+
+      else if (kb==67) {
+      	$(this).css("background-color", "#ff8c00 ");
+         
+         kb++;
+      }
+
+      else if (kb==68) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==69) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+
+      else if (kb==70) {
+          $(this).css("background-color", "#ff8c00 ");
+          kb++;
+      }
+      
+
+      else if (kb==71) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==72) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==73) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==74) {
+          $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==75) {
+         $(this).css("background-color", "#ff8c00 ");
+         kb++;
+      }
+
+      else if (kb==76) {
+      	$(this).animate({opacity: '.1'}, 1000);
+         
+         kb=0;
+      }
+
+     
+  });
+
+
+ var ad = Math.round(Math.random() * 255);
+var bd = Math.round(Math.random() * 255);
+var cd = Math.round(Math.random() * 255);
+ $( "body" ).css("background-color", "RGB(" + ad + "," + bd + "," + cd + ")");
+ $( "kb" ).css("background-color", "RGB(" + ad + "," + bd + "," + cd + ")");
+
+
+	$('#box1').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+
+	$('#box3').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+	$('#box4').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+	$('#box5').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	
+		
+	});
+
+
+
+	$('#box6').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	}); 
+
+
+	$('#box7').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+
+	$('#box8').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+	$('#box9').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+
+	$('#box10').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+	$('#box11').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+	$('#box12').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	
+	});
+
+
+	$('#box13').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+	$('#box14').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+
+	$('#box15').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+	$('#box16').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+	$('#box17').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	}); 
+
+
+	$('#box18').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+
+	});
+
+
+	$('#box19').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+
+	$('#box20').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+
+
+
+
+	$('#box21').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+
+	$('#box22').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+	$('#box23').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+
+	$('#box24').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+	$('#box25').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	
+		
+	});
+
+
+
+	$('#box26').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	}); 
+
+
+	$('#box27').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+
+	$('#box28').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+	$('#box29').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+
+	$('#box30').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+	$('#box31').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+	$('#box32').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	
+	});
+
+
+	$('#box33').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+	$('#box34').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+
+	$('#box35').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+	$('#box36').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+	$('#box37').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	}); 
+
+
+	$('#box38').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+
+	});
+
+
+	$('#box39').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+
+	$('#box40').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+		$('#box41').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+
+	$('#box42').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+	$('#box43').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+
+	$('#box44').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+	$('#box45').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	
+		
+	});
+
+
+
+	$('#box46').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	}); 
+
+
+	$('#box47').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+
+	$('#box48').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+	$('#box49').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+
+	$('#box50').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+	$('#box51').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+	$('#box52').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	
+	});
+
+
+	$('#box53').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+	$('#box54').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+
+	$('#box55').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+	$('#box56').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+	$('#box57').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	}); 
+
+
+	$('#box58').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+
+	});
+
+
+	$('#box59').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+
+	$('#box60').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+		$('#box61').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+	$('#box62').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	
+	});
+
+
+	$('#box63').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+	$('#box64').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+
+	$('#box65').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+	$('#box66').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+
+	$('#box67').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	}); 
+
+
+	$('#box68').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+
+	});
+
+
+	$('#box69').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+
+	$('#box70').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+		$('#box71').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+
+	$('#box72').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+	$('#box73').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	});
+
+
+	$('#box74').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+		
+	});
+
+
+	$('#box75').hover(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+	
+		
+	});
+
+
+
+});
